7f43811225fb9fe001772c3d4cfbe207f3a89770
BRITVA-hp <borbritvahp@gmail.com>
2022-09-22 13:58:39 +0400

    calc layout

--- js/script.js
+++ js/script.js
@@ -25,18 +25,36 @@ document.addEventListener('DOMContentLoaded', () => {
               rangeTrack = document.querySelector(rangeTrack_),
               rangeNum = document.querySelector(rangeNum_);
 
+        
+        let min = +rangeInput.getAttribute('min'),
+            max = +rangeInput.getAttribute('max'),
+            step = +rangeInput.getAttribute('step'),
+            val_ = +rangeInput.value;
+
         rangeInput.addEventListener('input', function() {
-            let val = +this.value,
-                min = +this.getAttribute('min'),
-                max = +this.getAttribute('max'),
-                step = +this.getAttribute('step'),
+            let val = +rangeInput.value,
                 position = 100 / (max - step) * (val - step);
 
             rangeTrack.style.width = `${position}%`;
-            rangeNum.textContent = `${val}`;
+            rangeNum.value = `${val}`;
         });
+
+        rangeNum.addEventListener('input', function () {
+            this.value = this.value.replace(/\D/g, '')
+            if (this.value > max) {
+                this.value = max
+            }
+            if(this.value < min) {
+                return
+            }
+            if (this.value > min && this.value <= max)  {
+                rangeTrack.style.width = `${100 / (max - step) * (this.value - step)}%`;
+                val_ = this.value   
+            }
+        })
     }
 
-    range(".calc__range__input--1", ".calc__range__track--1", ".calc__field__text--1")
+    range(".calc__range__input--1", ".calc__range__track--1", ".calc__field--1")
+    range(".calc__range__input--2", ".calc__range__track--2", ".calc__field--2")
 
 })
